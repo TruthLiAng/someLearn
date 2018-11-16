7013bea604c2ea770fc3819e8a9a8b9df0479d54
zhouliang <zhou@truth>
2018-11-16 11:36:54 +0800

    增加typescript模板

---
+++ TypeScript/README.md
@@ -0,0 +1,1 @@
+# typescript 模板
---
+++ TypeScript/dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greet_1 = require("./greet");
+function showHello(divName, name) {
+    var elt = document.getElementById(divName);
+    elt.innerText = greet_1.sayHello(name);
+}
+showHello("greeting", "ts");
+console.log(greet_1.sayHello("TypeScript"));
